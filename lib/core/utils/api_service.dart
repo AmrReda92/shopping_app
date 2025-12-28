d302522040d39f6f1377d1b0f18defe499a3cc27
Amr Reda <amr.reda2014@yahoo.com>
2025-12-28 02:35:54 +0200

    finish home repos and api service

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio _dio;
+  ApiService(this._dio);
+
+  final String _url = "https://fakestoreapi.com/products/";
+
+  Future<Map<String,dynamic>> get({required String endPoint})async{
+    final response = await _dio.get("$_url$endPoint");
+    return response.data;
+  }
+}
---
+++ lib/features/product/data/repos/product_repo_impl.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:shopping_app/core/errors/failure.dart';
+import 'package:shopping_app/core/utils/api_service.dart';
+import 'package:shopping_app/features/product/data/model/product_model.dart';
+import 'package:shopping_app/features/product/data/repos/product_repo.dart';
+
+class ProductModelImpl implements ProductRepo {
+  final ApiService apiService ;
+
+  ProductModelImpl(this.apiService);
+
+  @override
+  Future<Either<Failure, List<ProductModel>>> getAllProducts() async{
+   try{
+     final data = await apiService.get(endPoint: "products");
+     List<ProductModel> products =[];
+     for(var item in data as List){
+      products.add(ProductModel.fromJson(item));
+     }
+     return (right(products));
+   }catch(e){
+     return (left(ServerFailure("Something error, try later")));
+   }
+  }
+
+}
